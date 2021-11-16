c3ac5aaf3c38f6ce1d6b5064b246ac578369a1fe
Mohammed Hoque <mhoque@signifyhealth.com>
2021-11-16 08:38:40 -0600

    Initial check in

---
+++ Demo.Akka/NotificationActor.cs
@@ -0,0 +1,41 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Akka
+{
+    public class NotificationActor : UntypedActor
+    {
+        private IEmailNotification emailNotification;
+
+        public NotificationActor(IEmailNotification emailNotification) 
+        { 
+            this.emailNotification = emailNotification;
+        }
+
+        protected override void OnReceive(object message)
+        {
+            Console.WriteLine($"Message received: {message}");
+
+            if (message != null) 
+            {
+                emailNotification.Send(message.ToString());
+            }            
+        }
+
+        protected override void PreStart()
+        {
+            Console.WriteLine($"{nameof(NotificationActor)} started.");
+            base.PreStart();
+        }
+
+        protected override void PostStop()
+        {
+            Console.WriteLine($"{nameof(NotificationActor)} stopped.");
+            base.PostStop();
+        }
+    }
+}
---
+++ Demo.Akka/Program.cs
@@ -0,0 +1,20 @@
+﻿using Akka.Actor;
+using Akka.DI.Core;
+using Akka.DI.Extensions.DependencyInjection;
+using Demo.Akka;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+services.AddSingleton<IEmailNotification, EmailNotification>();
+services.AddSingleton<NotificationActor>();
+var provider = services.BuildServiceProvider();
+
+using var actorSystem = ActorSystem.Create("test-actor-system");
+actorSystem.UseServiceProvider(provider);
+
+var actor = actorSystem.ActorOf(actorSystem.DI().Props<NotificationActor>());
+actor.Tell("Hello World!!!");
+actorSystem.Stop(actor);
+
+Console.WriteLine("\n\nPress any key to exit.");
+Console.Read();
